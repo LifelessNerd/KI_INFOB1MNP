dfd55d13d76f9a8d7da32aefc62c500b08003a2e
LifelessNerd <35869162+LifelessNerd@users.noreply.github.com>
2021-11-29 17:04:29 +0100

    na hoorcollege
    29.11

---
+++ 29-11-Hoorcollege/29-11-Hoorcollege/Program.cs
@@ -0,0 +1,48 @@
+﻿
+string veelwerf(string tekst, int aantal)
+{
+
+	string output = "";
+	int x = 0;
+	while (x < aantal)
+	{
+
+		output = output + tekst;
+		x++;
+	}
+	return output;
+}
+
+int machtsverheffing(int grondtal, int exponent)
+{
+	int x = 0;
+	int result = 1;
+	while (x < exponent)
+    {
+		result = result * grondtal;
+		x++;
+		
+	}
+	return result;
+}
+
+
+bool deelbaar(int x, int d)
+{
+
+	return x % d == 0;
+}
+
+int kleinsteDeler(int input)
+{
+	int x = 2;
+	int output;
+	while (input % x != 0)
+	{
+		x++;
+	}
+	return x;
+}
+
+Console.WriteLine(deelbaar(10,3));
+Console.WriteLine(kleinsteDeler(51));
